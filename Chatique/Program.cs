eb42438257b49d87cb14bd0b7215758a11a1ad7f
meh-d-flat <31995784+meh-d-flat@users.noreply.github.com>
2022-02-16 17:02:37 +0300

    Initial upload
    Via GitHub desktop

---
+++ Chatique/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//using SuperSocket.SocketBase.Config;
+//using SuperWebSocket;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Chatique
+{
+    public class Laputa : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Sessions.Broadcast(e.Data);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer(8087);
+
+            wssv.AddWebSocketService<Laputa>("/");
+            wssv.Start();
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
---
+++ ChatiqueClient/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using WebSocketSharp;
+
+namespace ChatiqueClient
+{
+    class Program
+    {
+        static string name;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("state your name");
+            name = Console.ReadLine();
+
+            using (var ws = new WebSocket(args.Length != 0 ? "ws://"+ args[0] +":8087/" : "ws://localhost:8087/"))
+            {
+                ws.OnMessage += (sender, e) =>
+                    Console.WriteLine(e.Data);
+
+                ws.Connect();
+                while (true)
+                {
+                    var message = Console.ReadLine();
+                    if (Console.CursorTop != 0)
+                    {
+                        Console.SetCursorPosition(0, Console.CursorTop - 1);
+                        Console.Write(new string(' ', Console.WindowWidth));
+                        Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    }
+                    ws.Send(String.Format("[{0}]: {1}", name, message));
+                }
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
