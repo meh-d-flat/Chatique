92444c95dd393c92431fe5e7c56a8ba66000bbee
meh-d-flat <31995784+meh-d-flat@users.noreply.github.com>
2022-04-01 23:19:50 +0300

    Custom auth with form
    Just a stub for now

---
+++ ChatiqueWebGateway/CustomAuth.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web;
+
+namespace ChatiqueWebGateway
+{
+    class CustomAuth
+    {
+        public void Go()
+        {
+            using (HttpListener listener = new HttpListener())
+            {
+                listener.Prefixes.Add(@"http://+:9090/sup/");
+                listener.Start();
+
+                while (true)
+                {
+                    var c = listener.GetContext();
+
+                    if (!c.Request.TryGetRequestCookie("beenBefore"))
+                    {
+                        c.Response.Cookies.Add(new Cookie("beenBefore", "true"));
+                        WriteResponse(c.Response, "*Login form*");
+                    }
+
+                    if (c.Request.TryGetRequestCookie("authenticated"))
+                        WriteResponse(c.Response, "*Chatique page*");
+
+                    else
+                    {
+                        Console.WriteLine("is not authenticated");
+                        using (var reader = new StreamReader(c.Request.InputStream, c.Request.ContentEncoding))
+                        {
+                            var parsed = HttpUtility.ParseQueryString(reader.ReadToEnd());
+
+                            if (parsed["username"] == null || parsed["username"] == String.Empty)
+                                WriteResponse(c.Response, "*Login form*");
+
+                            else
+                            {
+                                var username = new Cookie("name", parsed["username"]);
+                                c.Response.Cookies.Add(new Cookie("authenticated", "true"));
+                                c.Response.Cookies.Add(username);
+
+                                c.Response.StatusCode = 303;
+                                c.Response.RedirectLocation = c.Request.Url.ToString();
+                                WriteResponse(c.Response, "Logging you in");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        void WriteResponse(HttpListenerResponse response, string text)
+        {
+            using (var writer = new StreamWriter(response.OutputStream))
+                writer.Write(text);
+        }
+    }
+}
