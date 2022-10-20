a13c9230d59babb2dc45c8507ce51a77b4eea3fa
meh-d-flat <31995784+meh-d-flat@users.noreply.github.com>
2022-10-20 19:52:42 +0300

    sign up basic form

--- Chatique/Vault.cs
+++ Chatique/Vault.cs
@@ -1,20 +1,27 @@
 ﻿using System;
+using System.Configuration;
+using System.Management;
+using System.Collections.Specialized;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
+using System.Data;
 
 namespace Chatique
 {
     public class Vault
     {
+        static readonly string dbFile = ConfigurationManager.AppSettings["dbFile"].ToString();
+        static readonly string connectionString = ConfigurationManager.AppSettings["connectionString"].ToString();
+
         public static void Configure()
         {
-            if (!File.Exists("database.sqlite3"))
-                SQLiteConnection.CreateFile("database.sqlite3");
+            if (!File.Exists(dbFile))
+                SQLiteConnection.CreateFile(dbFile);
 
-            using (var connection = new SQLiteConnection("Data Source=database.sqlite3"))
+            using (var connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
                 string createTableQuery =
@@ -28,7 +35,7 @@ namespace Chatique
                     CREATE TABLE IF NOT EXISTS Users
                     (
                         ID INTEGER PRIMARY KEY AUTOINCREMENT,
-                        Username VARCHAR(255) NOT NULL,
+                        Username VARCHAR(255) NOT NULL UNIQUE,
                         Password VARCHAR(255) NOT NULL
                     )";
                 using (SQLiteCommand createTable = new SQLiteCommand(createTableQuery, connection))
@@ -41,14 +48,14 @@ namespace Chatique
         public static SQLiteException AddMessageToDB(Post message)
         {
             SQLiteException sqlException = null;
-            using (var connection = new SQLiteConnection("Data Source=database.sqlite3"))
+            using (var connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
                 string addMessageQuery = "INSERT INTO Posts ('Timestamp', 'Message', 'Username') VALUES(@a, @b, @c)";
                 SQLiteCommand addMessage = new SQLiteCommand(addMessageQuery, connection);
-                addMessage.Parameters.AddWithValue("@a", message.Timestamp);
-                addMessage.Parameters.AddWithValue("@b", message.Message);
-                addMessage.Parameters.AddWithValue("@c", message.Username);
+                addMessage.Parameters.Add(new SQLiteParameter() { ParameterName = "@a", Value = message.Timestamp, DbType = DbType.DateTime });
+                addMessage.Parameters.Add(new SQLiteParameter() { ParameterName = "@b", Value = message.Message, DbType = DbType.String });
+                addMessage.Parameters.Add(new SQLiteParameter() { ParameterName = "@c", Value = message.Username, DbType = DbType.String});
                 try
                 {
                     addMessage.ExecuteNonQuery();
@@ -71,7 +78,7 @@ namespace Chatique
         public static List<Post> GetAllMessages()
         {
             var allPosts = new List<Post>();
-            using (var connection = new SQLiteConnection("Data Source=database.sqlite3"))
+            using (var connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
                 string historyQuery = "SELECT Timestamp, Username, Message FROM Posts";
@@ -89,7 +96,7 @@ namespace Chatique
             return allPosts;
         }
 
-        public static bool CheckCredentialHashing(string username, string password)
+        public static string MakeHash(string password)
         {
             string hash = String.Empty;
             using (SHA512 sha = SHA512Managed.Create())
@@ -98,25 +105,90 @@ namespace Chatique
                 hash = hash.Replace("-", "");
                 hash = hash.ToLower();
             }
+            return hash;
+        }
+
+        public static bool CheckCredentialHashing(string username, string password)
+        {
+            string hash = MakeHash(password);
             return CheckCredential(username, hash);
         }
 
         public static bool CheckCredential(string username, string password)
         {
             bool exists = false;
-            using (var connection = new SQLiteConnection("Data Source=database.sqlite3"))
+            using (var connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
                 string credentialQuery = "SELECT COUNT(*) FROM Users WHERE Username = @a AND Password = @b";
                 using (SQLiteCommand credential = new SQLiteCommand(credentialQuery, connection))
                 {
-                    credential.Parameters.AddWithValue("@a", username);
-                    credential.Parameters.AddWithValue("@b", password/*hash*/);
+                    credential.Parameters.Add(new SQLiteParameter() { ParameterName = "@a", Value = username, DbType = DbType.String });
+                    credential.Parameters.Add(new SQLiteParameter() { ParameterName = "@b", Value = password, DbType = DbType.String });//AddWithValue("@b", password/*hash*/);
                     exists = Convert.ToBoolean(credential.ExecuteScalar());
+                    credential.Dispose();
                 }
                 connection.Close();
             }
             return exists;
+        }
+
+        public static bool CreateCredential(string username, string password)
+        {
+            string hash = MakeHash(password);
+            bool completed = false;
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string credentialQuery = "INSERT INTO Users ('Username', 'Password') VALUES(@a, @b)";
+                using (SQLiteCommand credential = new SQLiteCommand(credentialQuery, connection))
+                {
+                    credential.Parameters.Add(new SQLiteParameter() { ParameterName = "@a", Value = username, DbType = DbType.String });
+                    credential.Parameters.Add(new SQLiteParameter() { ParameterName = "@b", Value = hash, DbType = DbType.String });
+
+                    try
+                    {
+                        completed = Convert.ToBoolean(credential.ExecuteNonQuery());
+                    }
+                    catch(SQLiteException ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                    credential.Dispose();
+                }
+                connection.Close();
+            }
+            return completed;
+        }
+
+        public static bool CreateCredential2(string username, string password)
+        {
+            bool created = false;
+            string hash = MakeHash(password);
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                string addMessageQuery = "INSERT INTO Users ('Username', 'Password') VALUES(@a, @b)";
+                SQLiteCommand addMessage = new SQLiteCommand(addMessageQuery, connection);
+                addMessage.Parameters.Add(new SQLiteParameter() { ParameterName = "@a", Value = username, DbType = DbType.String });
+                addMessage.Parameters.Add(new SQLiteParameter() { ParameterName = "@b", Value = hash, DbType = DbType.String });
+                try
+                {
+                    var result = addMessage.ExecuteNonQuery();
+                    created = Convert.ToBoolean(result);
+                }
+                catch (SQLiteException ex)
+                {
+                    Console.WriteLine(ex);
+                    addMessage.Transaction.Rollback();
+                }
+                finally
+                {
+                    addMessage.Dispose();
+                }
+                connection.Close();
+            }
+            return created;
         }
     }
 }
--- ChatiqueWebGateway/CustomAuth.cs
+++ ChatiqueWebGateway/CustomAuth.cs
@@ -24,16 +24,77 @@ namespace ChatiqueWebGateway
                         Extensions.HtmlResponse(context.Response, "Login.html");
                     }
 
+                    if (context.Request.TryGetRequestCookie("signup"))
+                    {
+                        context.Request.Cookies["signup"].Expires = DateTime.Now.AddSeconds(5);
+                        context.Request.Cookies["signup"].Expired = true;
+                        //context.Response.Cookies["signup"].Expired = true;
+                        context.Response.Cookies.Add(new Cookie("signing-in", "true"));
+                        Extensions.HtmlResponse(context.Response, "Signup.html");
+                        continue;
+                    }
+
                     if (context.Request.TryGetRequestCookie("authenticated"))
                         Extensions.HtmlResponse(context.Response, "Index.html");
 
+                    else if (context.Request.TryGetRequestCookie("signing-in"))
+                        ProcessSignUpForm(context);
+
                     else
-                        ProcessForm(context);
+                        ProcessLoginForm(context);
                 }
             }
         }
 
-        void ProcessForm(HttpListenerContext ctx)
+        void ProcessSignUpForm(HttpListenerContext ctx)
+        {
+            using (var reader = new StreamReader(ctx.Request.InputStream, ctx.Request.ContentEncoding))
+            {
+                var parsed = HttpUtility.ParseQueryString(reader.ReadToEnd());
+
+                ctx.Response.Cookies.Add(new Cookie("signing-in", "true"));
+
+                if (parsed.Count == 0)
+                {
+                    Extensions.HtmlResponse(ctx.Response, "Signup.html");
+                    //return;
+                }
+
+                if (parsed["username"] == null || parsed["username"] == String.Empty)
+                    Extensions.HtmlResponse(ctx.Response, "Signup.html");
+
+                if (parsed["password"] == null || parsed["password"] == String.Empty)
+                    Extensions.HtmlResponse(ctx.Response, "Signup.html");
+
+                if (parsed["password"] != parsed["repeat-password"])
+                    Extensions.HtmlResponse(ctx.Response, "Signup.html");
+
+                //if//else
+                //{
+                    if (Chatique.Vault.CreateCredential2(parsed["username"], parsed["password"]))
+                    {
+                        Console.WriteLine("done signing up");
+                    var username = new Cookie("name", parsed["username"]);
+                    ctx.Response.Cookies.Add(new Cookie("authenticated", "true"));
+                    ctx.Response.Cookies.Add(username);
+                    ctx.Response.StatusCode = 303;
+                    ctx.Response.RedirectLocation = ctx.Request.Url.ToString();
+                    Extensions.WriteResponse(ctx.Response, "Logging you in");
+                    //var username = new Cookie("name", parsed["username"]);
+                    //ctx.Response.Cookies.Add(new Cookie("authenticated", "true"));
+                    //ctx.Response.Cookies.Add(username);
+                    //ctx.Response.StatusCode = 303;
+                    //ctx.Response.RedirectLocation = ctx.Request.Url.ToString();
+                    //Extensions.WriteResponse(ctx.Response, "Logging you in");
+                }
+                    else
+                        Extensions.WriteResponse(ctx.Response, "Something went wrong!");
+                    //Extensions.HtmlResponse(ctx.Response, "Login.html");
+                //}
+            }
+        }
+
+        void ProcessLoginForm(HttpListenerContext ctx)
         {
             using (var reader = new StreamReader(ctx.Request.InputStream, ctx.Request.ContentEncoding))
             {
